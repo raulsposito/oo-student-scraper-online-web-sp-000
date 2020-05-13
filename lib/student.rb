7663725c40e2ac6e9cdc2bd51a0643a0f7bdb2b1
Raul Sposito <raulsposito@gmail.com>
2020-05-13 21:00:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |student_hash|
-      Student.new 
+      Student.new(student_hash)
   end
 
   def add_student_attributes(attributes_hash)
