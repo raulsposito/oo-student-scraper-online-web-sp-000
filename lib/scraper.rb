f03430101ac5d949c062b55065626be476a53d8d
Raul Sposito <raulsposito@gmail.com>
2020-05-13 18:45:45 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,11 +33,8 @@ class Scraper
       end
     end
     student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css (".profile-quote")
-    student[:bio] = profile_page.css("")
-    
-    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
     student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
-
+    
     student
     
   end
