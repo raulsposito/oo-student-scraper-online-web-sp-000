11affcbb1884b67dd4013bbde69625a567628f39
Raul Sposito <raulsposito@gmail.com>
2020-05-13 17:48:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -24,7 +24,7 @@ class Scraper
     links.each do |link|
       if link.include?("linkedin")
         student[:linkedin] = link 
-      
+      elsif 
     
     profile_page = Nokogiri::HTML(open(profile_url))
     profile = []
