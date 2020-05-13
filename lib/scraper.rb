97f64cca38b1ebe3d25f39acda044d45ebeb581d
Raul Sposito <raulsposito@gmail.com>
2020-05-13 17:52:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
         student[:github] = link 
       elsif link.include?("twitter")
         student[:twitter] = link
-      
+      else 
     
     profile_page = Nokogiri::HTML(open(profile_url))
     profile = []
