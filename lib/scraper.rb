756a647f36baed08a79b64017dfb44a674758869
Raul Sposito <raulsposito@gmail.com>
2020-05-13 17:44:17 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,8 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    students = 
+    students = {}
+    
     
     profile_page = Nokogiri::HTML(open(profile_url))
     profile = []
